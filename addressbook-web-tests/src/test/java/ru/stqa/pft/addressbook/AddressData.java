4aaacf98984a99cbb24a23ea25b657aa2ab07e21
shaikin1 <35539368+shaikin1@users.noreply.github.com>
2018-03-03 13:21:51 +0400

    test of creating contact

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddressData.java
@@ -0,0 +1,25 @@
+package ru.stqa.pft.addressbook;
+
+public class AddressData {
+  private final String street;
+  private final String street_city;
+  private final String street_city_country;
+
+  public AddressData(String street, String street_city, String street_city_country) {
+    this.street = street;
+    this.street_city = street_city;
+    this.street_city_country = street_city_country;
+  }
+
+  public String getStreet() {
+    return street;
+  }
+
+  public String getStreet_city() {
+    return street_city;
+  }
+
+  public String getStreet_city_country() {
+    return street_city_country;
+  }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -0,0 +1,145 @@
+package ru.stqa.pft.addressbook;
+
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+
+public class ContactCreationTests {
+  FirefoxDriver wd;
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/group.php");
+    login("secret", "admin");
+  }
+
+  private void login(String password, String username) {
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+  }
+
+  @Test
+  public void testContactCreation() {
+
+    initContactCreation();
+    nameContactCreation("Sergei");
+    surnameContactCreation("Shaikin");
+    jobContactCreation("QA Engeneer");
+    companyContactCreation("Google");
+    addressContactCreation(new AddressData("Sennaya 86", "Sennaya 86\nDimitrovgrad", "Sennaya 86\nDimitrovgrad\nRussia"));
+    homephoneContactCreation("88423575134");
+    mobilephoneContactCreation("89876391174");
+    emailContactCreation("sashaikin@yandex.ru");
+    dobContactCreation("1976");
+    groupContactCreation();
+    homepageContactCreation();
+  }
+
+  private void homepageContactCreation() {
+    wd.findElement(By.linkText("home")).click();
+  }
+
+  private void groupContactCreation() {
+    if (!wd.findElement(By.xpath("//div[@id='content']/form/select[5]//option[2]")).isSelected()) {
+      wd.findElement(By.xpath("//div[@id='content']/form/select[5]//option[2]")).click();
+    }
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  private void dobContactCreation(String year) {
+    if (!wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[15]")).isSelected()) {
+      wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[15]")).click();
+    }
+    if (!wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[2]")).isSelected()) {
+      wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[2]")).click();
+    }
+    wd.findElement(By.name("byear")).click();
+    wd.findElement(By.name("byear")).clear();
+    wd.findElement(By.name("byear")).sendKeys(year);
+  }
+
+  private void emailContactCreation(String email) {
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(email);
+  }
+
+  private void mobilephoneContactCreation(String mobile) {
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(mobile);
+  }
+
+  private void homephoneContactCreation(String home) {
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(home);
+  }
+
+  private void addressContactCreation(AddressData addressData) {
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(addressData.getStreet());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(addressData.getStreet_city());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(addressData.getStreet_city_country());
+  }
+
+  private void companyContactCreation(String company) {
+    wd.findElement(By.name("company")).click();
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(company);
+  }
+
+  private void jobContactCreation(String title) {
+    wd.findElement(By.name("title")).click();
+    wd.findElement(By.name("title")).clear();
+    wd.findElement(By.name("title")).sendKeys(title);
+  }
+
+  private void surnameContactCreation(String lastname) {
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(lastname);
+  }
+
+  private void nameContactCreation(String firstname) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(firstname);
+  }
+
+  private void initContactCreation() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    wd.quit();
+  }
+
+  public static boolean isAlertPresent(FirefoxDriver wd) {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+}
