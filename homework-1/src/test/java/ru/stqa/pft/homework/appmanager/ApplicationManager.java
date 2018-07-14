1a1ee0b7921a97a149a7b1a59268ac6cc9cc3a95
shaikin1 <35539368+shaikin1@users.noreply.github.com>
2018-07-14 08:07:29 +0400

    Lesson 3.3 (Added browsers Crome and IE)

--- homework-1/src/test/java/ru/stqa/pft/homework/appmanager/ApplicationManager.java
+++ homework-1/src/test/java/ru/stqa/pft/homework/appmanager/ApplicationManager.java
@@ -1,19 +1,34 @@
 package ru.stqa.pft.homework.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-  private FirefoxDriver wd;
+  WebDriver wd;
 
   private SessionHelper sessionHelper;
   private ContactHelper contactHelper;
   private GroupHelper groupHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+    if (browser == BrowserType.FIREFOX) {
+      wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+    }    else if (browser == BrowserType.CHROME) {
+      wd = new ChromeDriver();
+    } else if (browser == BrowserType.IE) {
+      wd = new InternetExplorerDriver();
+    }
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     contactHelper = new ContactHelper(wd);
--- homework-1/src/test/java/ru/stqa/pft/homework/tests/TestBase.java
+++ homework-1/src/test/java/ru/stqa/pft/homework/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft.homework.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.homework.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  public final ApplicationManager app = new ApplicationManager();
+  public final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod
   public void setUp() throws Exception {
