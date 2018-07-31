44d09724ee0e71a225ee3e23a53fca84e5bb4b2b
shaikin1 <35539368+shaikin1@users.noreply.github.com>
2018-07-31 08:04:34 +0400

    Lesson 4.5 (forming of the collections)

--- homework-1/src/test/java/ru/stqa/pft/homework/tests/GroupModification.java
+++ homework-1/src/test/java/ru/stqa/pft/homework/tests/GroupModification.java
@@ -4,22 +4,24 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.homework.model.GroupData;
 
+import java.util.List;
+
 public class GroupModification extends TestBase {
 
   @Test
 
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
-    app.getGroupHelper().selectAnyGroup(before - 1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectAnyGroup(before.size() - 1);
     app.getGroupHelper().editSelectedGroup();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
     app.getGroupHelper().updateGroupEdition();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size());
   }
 }
