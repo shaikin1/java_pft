b925e6393da5b81a268f4b27fd5c11e36b154dac
shaikin1 <35539368+shaikin1@users.noreply.github.com>
2018-08-03 08:03:49 +0400

    Lesson 4.7 (irregular collections)

--- homework-1/src/test/java/ru/stqa/pft/homework/tests/GroupModification.java
+++ homework-1/src/test/java/ru/stqa/pft/homework/tests/GroupModification.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.homework.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModification extends TestBase {
@@ -18,10 +19,15 @@ public class GroupModification extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectAnyGroup(before.size() - 1);
     app.getGroupHelper().editSelectedGroup();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1", "test2", "test3");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().updateGroupEdition();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
