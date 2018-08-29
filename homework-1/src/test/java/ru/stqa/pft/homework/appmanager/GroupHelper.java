3a97ad70bbe052bf31c879c96090d61cd608342a
shaikin1 <35539368+shaikin1@users.noreply.github.com>
2018-08-29 07:35:11 +0400

    Lesson 5.7 (hashing of results of long-term operations)

--- homework-1/src/test/java/ru/stqa/pft/homework/appmanager/GroupHelper.java
+++ homework-1/src/test/java/ru/stqa/pft/homework/appmanager/GroupHelper.java
@@ -58,6 +58,7 @@ public class GroupHelper extends HelperBase {
     addNewGroup();
     fillGroupForm(group);
     submitGroupCreation();
+    groupCache = null;
     returnToGroupPage();
 
   }
@@ -67,12 +68,14 @@ public class GroupHelper extends HelperBase {
     editSelectedGroup();
     fillGroupForm(group);
     updateGroupEdition();
+    groupCache = null;
     returnToGroupPage();
   }
 
   public void delete(GroupData group) {
     selectAnyGroupById(group.getId());
     deleteSelectedGroup();
+    groupCache = null;
     returnToGroupPage();
   }
 
@@ -84,15 +87,20 @@ public class GroupHelper extends HelperBase {
     return wd.findElements(By.name("selected[]")).size();
   }
 
+  private Groups groupCache = null;
+
   public Groups all() {
-    Groups groups = new Groups();
+    if (groupCache != null) {
+      return new Groups(groupCache);
+    }
+    groupCache = new Groups();
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements) {
       String name = element.getText();
       int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-      groups.add(new GroupData().withId(id).withName(name));
+      groupCache.add(new GroupData().withId(id).withName(name));
     }
-    return groups;
+    return new Groups(groupCache);
   }
 
 }
