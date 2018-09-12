9a6e9750cf88d5bc88b858172b1fc4a34d9ac8ad
shaikin1 <35539368+shaikin1@users.noreply.github.com>
2018-09-12 09:45:59 +0400

    Lesson 5.8 (hashing and preliminary checks)

--- homework-1/src/test/java/ru/stqa/pft/homework/tests/GroupModification.java
+++ homework-1/src/test/java/ru/stqa/pft/homework/tests/GroupModification.java
@@ -25,8 +25,8 @@ public class GroupModification extends TestBase {
     GroupData modifiedGroup = before.iterator().next();
     GroupData group = new GroupData().withId(modifiedGroup.getId()).withName("test1").withHeader("test2").withFooter("test3");
     app.group().modify(group);
+    assertThat(app.group().count(), equalTo(before.size()));
     Groups after = app.group().all();
-    assertEquals(after.size(), before.size());
     assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
   }
 
