473244f46aefd796636fbe67f9dd15f689c6bc2b
shaikin1 <35539368+shaikin1@users.noreply.github.com>
2018-11-09 12:38:33 +0300

    Lesson 6.2 (generator of test data, record of data to the file)

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/CalculatorTest.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft.sandbox;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        double result = calculator.add(10,50);
+        if (result != 60) {
+            System.out.println("Bad result: " + result);
+        }
+    }
+}
