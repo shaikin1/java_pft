9f38378be78bf52f52a73278a78d3db8c8b653c2
shaikin1 <van76329@yandex.ru>
2019-01-14 15:23:35 +0300

    Lesson 6.6 (format XML)

--- homework-1/src/test/java/ru/stqa/pft/homework/generators/GroupDataGenerator.java
+++ homework-1/src/test/java/ru/stqa/pft/homework/generators/GroupDataGenerator.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.homework.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
 import ru.stqa.pft.homework.model.GroupData;
 
 import java.io.File;
@@ -20,6 +22,9 @@ public class GroupDataGenerator {
     @Parameter(names = "-f", description = "Target file")
     public String file;
 
+    @Parameter(names = "-d", description = "Data format")
+    public String format;
+
 
     public static void main(String[] args) throws IOException {
         GroupDataGenerator generator = new GroupDataGenerator();
@@ -33,23 +38,38 @@ public class GroupDataGenerator {
     }
 
     private void run() throws IOException {
-        List<GroupData> groups = generateGroups (count);
-        save(groups, new File(file));
+        List<GroupData> groups = generateGroups(count);
+        if (format.equals("csv")) {
+            saveAsCsv(groups, new File(file));
+        } else if (format.equals("xml")) {
+            saveAsXml(groups, new File(file));
+        } else {
+            System.out.println("Unrecognized format " + format);
+        }
     }
 
-    private void save(List<GroupData> groups, File file) throws IOException {
+    private void saveAsXml(List<GroupData> groups, File file) throws IOException {
+        XStream xstream = new XStream();
+        xstream.processAnnotations(GroupData.class);
+        String xml = xstream.toXML(groups);
+        Writer writer = new FileWriter(file);
+        writer.write(xml);
+        writer.close();
+    }
+
+    private void saveAsCsv(List<GroupData> groups, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
         for (GroupData group : groups) {
-            writer.write(String.format("%s,%s,%s\n", group.getName(), group.getHeader(), group.getFooter()));
+            writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
         }
         writer.close();
     }
 
-    private  List<GroupData> generateGroups(int count) {
+    private List<GroupData> generateGroups(int count) {
         List<GroupData> groups = new ArrayList<GroupData>();
         for (int i = 0; i < count; i++) {
-            groups.add(new GroupData().withName(String.format("test %s", i)).withHeader(String.format("header %s", i)).withFooter(String.format("footer %s", i)));
+            groups.add(new GroupData().withName(String.format("test %s", i)).withHeader(String.format("header\n%s", i)).withFooter(String.format("footer\n%s", i)));
         }
         return groups;
     }
