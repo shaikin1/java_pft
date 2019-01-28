2ea3efc6848b984774751496f900683eeac3d594
shaikin1 <van76329@yandex.ru>
2019-01-28 15:33:52 +0300

    Homework of task 12 (lesson 6.4)

--- homework-1/src/test/java/ru/stqa/pft/homework/tests/ContactCreationTests.java
+++ homework-1/src/test/java/ru/stqa/pft/homework/tests/ContactCreationTests.java
@@ -1,10 +1,14 @@
 package ru.stqa.pft.homework.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.homework.model.ContactData;
 import ru.stqa.pft.homework.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.junit.MatcherAssert.assertThat;
@@ -12,12 +16,20 @@ import static org.testng.Assert.assertEquals;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test(enabled = true)
-  public void testContactCreation() {
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new ContactData().withFirstname("firstname1").withLastname("lastname 1").withTitle("title 1").withCompany("company 1").withHome("home 1").withAddress("address 1").withEmail("email 1").withGroup("test 1")});
+    list.add(new Object[] {new ContactData().withFirstname("firstname2").withLastname("lastname 2").withTitle("title 2").withCompany("company 2").withHome("home 2").withAddress("address 2").withEmail("email 2").withGroup("test 2")});
+    list.add(new Object[] {new ContactData().withFirstname("firstname3").withLastname("lastname 3").withTitle("title 3").withCompany("company 3").withHome("home 3").withAddress("address 3").withEmail("email 3").withGroup("test 3")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) {
     app.goTo().gotoHomePage();
     Contacts before = app.contact().all();
     File photo = new File("src/test/resources/stru.png");
-    ContactData contact = new ContactData().withFirstname("Liliia").withLastname("Shaikina").withTitle("Designer").withCompany("Apple").withHome("+79876391173").withEmail("shaikinal@yahoo.com").withPhoto(photo).withGroup("test2");
     app.contact().create(contact, true);
     app.contact().returnToContactPage();
     Contacts after = app.contact().all();
